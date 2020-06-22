c451560404fb7133cbe1297f8ebbf6d1d8ad12a6
Angelo Dias <oiangelodias@gmail.com>
2020-06-22 10:40:57 -0300

    classToClass changes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -8,6 +8,11 @@ import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+  console.log('GOT');
+  res.json({ message: 'running!' });
+});
+
 routes.use('/appointments', appointmentsRouter);
 routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
